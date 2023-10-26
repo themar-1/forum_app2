e472d7b41f64d7702c7cdc0eb72bb78d8c009126
Yassine Farih <yassinefarih27@gmail.com>
2023-10-26 21:03:17 +0100

    adding migrations

---
+++ database/migrations/2023_10_16_204457_create_entretiens_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('entretiens', function (Blueprint $table) {
+            $table->id();
+            $table->string('stagiaire_id');
+            $table->string('entreprise_id');
+            $table->tinyInteger('status')->default(0);
+            $table->timestamps();
+            $table->foreign('stagiaire_id')->references('id')->on('stagiaires');
+            $table->foreign('entreprise_id')->references('id')->on('entreprises');
+            $table->unique(['stagiaire_id', 'entreprise_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('entretiens');
+    }
+};
