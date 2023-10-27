d4c371615582738eeec80459c8f387e7db0e5085
AKRAM <63755025+Skyreex@users.noreply.github.com>
2023-10-27 02:46:00 +0100

    erhzrjrzjz

--- app/Imports/EntreprisesImport.php
+++ app/Imports/EntreprisesImport.php
@@ -17,10 +17,15 @@ class EntreprisesImport implements ToModel
     public function model(array $row)
     {
         $ent = new Entreprise();
-        $ent->nom = $row[0];
-        $ent->representant = $row[1];
-        $ent->email = $row[2];
-        $ent->stand = $row[3];
+        $ent->email = $row[0];
+        $ent->web = $row[1];
+        $ent->nom = $row[2];
+        $ent->raisonsociale = $row[2];
+        $ent->raisonabregee = $row[3];
+        $ent->activite = $row[4];
+        $ent->representant = $row[5];
+        $ent->logo = $row[6];
+        $ent->stand = $row[7];
         return $ent;
     }
 }
